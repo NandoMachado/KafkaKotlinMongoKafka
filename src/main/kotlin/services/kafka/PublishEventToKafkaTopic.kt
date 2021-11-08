e58a02c670f5ec14a03ac65321be1061dc323264
Nando Machado <nando.carnielmachado@sainsburys.co.uk>
2021-11-08 11:42:34 +0000

    Added logger to publisher

--- src/main/kotlin/services/kafka/PublishEventToKafkaTopic.kt
+++ src/main/kotlin/services/kafka/PublishEventToKafkaTopic.kt
@@ -1,14 +1,29 @@
 package services.kafka
 
 import models.KafkaProducerModel
+import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class PublishEventToKafkaTopic {
-    fun publish(){
+    fun publish() {
         val producer = KafkaProducerModel().createProducer("localhost:29092")
         val record = ProducerRecord<String, String>("outboundTopic", "Outbound topic event")
+        val logger: Logger = LoggerFactory.getLogger("outboundProducer")
 
-        producer.send(record)
+        producer.send(record) { recordMetadata, e: Exception? ->
+            if (e == null) {
+                logger.info(
+                    "Successfully received the details as \n" +
+                            "Topic: ${ recordMetadata.topic() } \n" +
+                            "Offset: ${ recordMetadata.offset() } \n" +
+                            "Timestamp: ${ recordMetadata.timestamp() }"
+                )
+            } else {
+                logger.error("Can't produce, getting error $e")
+            }
+        }
         producer.flush()
         producer.close()
     }
